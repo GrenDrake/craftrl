37d31caa2c03767204a7ecc06395cfdccb28955e
Gren Drake <gren@grenslair.com>
2020-09-27 04:01:50 -0700

    Improves crafting interface

--- src/crafting.cpp
+++ src/crafting.cpp
@@ -1,8 +1,30 @@
 #include <BearLibTerminal.h>
 #include "world.h"
 
+bool canMakeRecipe(World &w, const RecipeDef *def, const Inventory &inventory) {
+    if (!def) return false;
+
+    for (const RecipeRow &row : def->mRows) {
+        const ItemDef &partDef = w.getItemDef(row.ident);
+        if (inventory.qty(&partDef) < row.qty) {
+            return false;
+        }
+    }
+    return true;
+}
 
 void doCrafting(World &w, Actor *player) {
+    const unsigned highlightBG  = 0xFF666666;
+    const unsigned highlightFG  = 0xFFFFFFFF;
+    const unsigned textBG       = 0xFF000000;
+    const unsigned textFG       = 0xFFAAAAAA;
+    const unsigned validFG      = 0xFFAAFFAA;
+    const unsigned invalidFG    = 0xFFFFAAAA;
+
+    const int screenWidth = 80;
+    const int screenHeight = 24;
+    const int inventoryHeight = 11;
+    const int inventoryY = screenHeight - inventoryHeight;
 
     const auto list = w.getRecipeList();
 
@@ -10,17 +32,36 @@ void doCrafting(World &w, Actor *player) {
     while (1) {
         const RecipeDef *current = nullptr;
 
+        terminal_bkcolor(textBG);
+        terminal_color(textFG);
         terminal_clear();
+        for (int y = 0; y < screenHeight; ++y) {
+            terminal_put(29, y, '|');
+        }
+        for (int x = 30; x < screenWidth; ++x) {
+            terminal_put(x, inventoryY - 1, '-');
+        }
+        terminal_put(29, inventoryY - 1, '+');
+
         int cy = 0;
         for (const auto *row : list) {
             if (!row) continue;
             const ItemDef &makeDef = w.getItemDef(row->makeIdent);
             if (cy == selection) {
-                terminal_print(0, cy, "-->");
-                terminal_color(0xFFFFFFFF);
+                terminal_color(highlightFG);
+                terminal_bkcolor(highlightBG);
+                terminal_clear_area(0, cy, 29, 1);
+                terminal_put(2, cy, '>');
                 current = row;
             } else {
-                terminal_color(0xFFAAAAAA);
+                terminal_color(textFG);
+                terminal_bkcolor(textBG);
+            }
+            if (canMakeRecipe(w, row, player->inventory))  {
+                terminal_color(0xFFAAFFAA);
+                terminal_put(0, cy, '+');
+            } else {
+                terminal_color(0xFFFFAAAA);
             }
             if (makeDef.ident >= 0) {
                 terminal_printf(4, cy, "%d %s", row->makeQty, makeDef.name.c_str());
@@ -30,40 +71,55 @@ void doCrafting(World &w, Actor *player) {
             ++cy;
         }
 
-        terminal_color(0xFFFFFFFF);
-        bool canMake = true;
+        terminal_color(textFG);
+        terminal_bkcolor(textBG);
+        int top = 0;
+        for (unsigned i = 0; i < inventoryHeight; ++i) {
+            unsigned index = i + top;
+            if (index < 0 || index >= player->inventory.mContents.size()) continue;
+            int iqty            = player->inventory.mContents[i].qty;
+            const ItemDef *idef = player->inventory.mContents[i].def;
+            if (idef == nullptr) {
+                terminal_printf(32, inventoryY + i, "    (bad item ident)");
+            } else {
+                terminal_printf(32, inventoryY + i, "%3d %s", iqty, idef->name.c_str());
+            }
+        }
+
+        bool canMake = canMakeRecipe(w, current, player->inventory);
         if (current) {
             cy = 0;
             for (const RecipeRow &row : current->mRows) {
                 const ItemDef &partDef = w.getItemDef(row.ident);
                 if (player->inventory.qty(&partDef) < row.qty) {
-                    canMake = false;
-                    terminal_color(0xFFFFAAAA);
+                    terminal_color(invalidFG);
                 } else {
-                    terminal_color(0xFFAAFFAA);
+                    terminal_color(validFG);
+                    terminal_put(30, cy, '+');
                 }
                 if (partDef.ident >= 0) {
-                    terminal_printf(30, cy, "%d %s", row.qty, partDef.name.c_str());
+                    terminal_printf(32, cy, "%d %s", row.qty, partDef.name.c_str());
                 } else {
-                    terminal_printf(30, cy, "Bad item ident: %s", row.ident);
+                    terminal_printf(32, cy, "Bad item ident: %s", row.ident);
                 }
                 ++cy;
             }
         }
 
-        terminal_color(0xFFFFFFFF);
-        terminal_printf(30, cy + 2, "Can make: %s", canMake ? "YES" : "NO");
         terminal_refresh();
 
         int key = terminal_read();
         switch(key) {
+            case TK_ESCAPE:
+            case TK_CLOSE:
             case TK_Z:
+            case TK_Q:
                 return;
             case TK_DOWN:
-                ++selection;
+                if (selection < static_cast<int>(list.size()) - 1) ++selection;
                 break;
             case TK_UP:
-                --selection;
+                if (selection > 0) --selection;
                 break;
             case TK_ENTER:
             case TK_KP_ENTER:
