f3628e7482e5239425a4118882c3d13827745a9e
Gren Drake <gren@grenslair.com>
2020-11-15 17:48:26 -0800

    Improves crafting UI

--- src/crafting.cpp
+++ src/crafting.cpp
@@ -3,16 +3,26 @@
 
 const int MAX_CRAFT = 1000000;
 
-bool canMakeRecipe(World &w, const RecipeDef *def, int qty, const Inventory &inventory) {
-    if (!def) return false;
+bool canMakeRecipe(World &w, const RecipeDef *recipe, int qty, const Inventory &inventory) {
+    if (!recipe) return false;
 
-    for (const RecipeRow &row : def->mRows) {
+    for (const RecipeRow &row : recipe->mRows) {
         const ItemDef &partDef = w.getItemDef(row.ident);
         if (inventory.qty(&partDef) < row.qty * qty) {
             return false;
         }
     }
     return true;
+}
+
+int maxCraftable(World &w, const RecipeDef *recipe, const Inventory &inventory) {
+    int maxCount = 0;
+    for (int i = 1; i < 99999; ++i) {
+        if (canMakeRecipe(w, recipe, i, inventory)) {
+            maxCount = i;
+        }
+    }
+    return maxCount;
 }
 
 void doCrafting(World &w, Actor *player, unsigned craftingStation) {
@@ -22,11 +32,7 @@ void doCrafting(World &w, Actor *player, unsigned craftingStation) {
     const unsigned textFG       = 0xFFAAAAAA;
     const unsigned validFG      = 0xFFAAFFAA;
     const unsigned invalidFG    = 0xFFFFAAAA;
-
-    const int screenWidth = 80;
-    const int screenHeight = 24;
-    const int inventoryHeight = 11;
-    const int inventoryY = screenHeight - inventoryHeight;
+    const int screenHeight = 25;
 
     const auto list = w.getRecipeList(craftingStation);
 
@@ -40,10 +46,6 @@ void doCrafting(World &w, Actor *player, unsigned craftingStation) {
         for (int y = 0; y < screenHeight; ++y) {
             terminal_put(29, y, LD_VERTICAL);
         }
-        for (int x = 30; x < screenWidth; ++x) {
-            terminal_put(x, inventoryY - 1, LD_HORIZONTAL);
-        }
-        terminal_put(29, inventoryY - 1, LD_TEE_RUD);
 
         int cy = 0;
         for (const auto *row : list) {
@@ -65,8 +67,9 @@ void doCrafting(World &w, Actor *player, unsigned craftingStation) {
                 terminal_color(0xFFFFAAAA);
             }
             if (makeDef.ident >= 0) {
-                terminal_put(2, cy, makeDef.glyph);
                 terminal_printf(4, cy, "%d %s", row->makeQty * count, makeDef.name.c_str());
+                terminal_color(0xFFFFFFFF);
+                terminal_put(2, cy, makeDef.glyph);
             } else {
                 terminal_printf(4, cy, "Bad item ident: %s", row->makeIdent);
             }
@@ -75,33 +78,22 @@ void doCrafting(World &w, Actor *player, unsigned craftingStation) {
 
         terminal_color(textFG);
         terminal_bkcolor(textBG);
-        terminal_printf(35, inventoryY - 1, " Crafting: %d ", count);
-        int top = 0;
-        for (unsigned i = 0; i < inventoryHeight; ++i) {
-            unsigned index = i + top;
-            if (index < 0 || index >= player->inventory.mContents.size()) continue;
-            int iqty            = player->inventory.mContents[i].qty;
-            const ItemDef *idef = player->inventory.mContents[i].def;
-            if (idef == nullptr) {
-                terminal_printf(32, inventoryY + i, "    (bad item ident)");
-            } else {
-                terminal_printf(32, inventoryY + i, "%3d %s", iqty, idef->name.c_str());
-            }
-        }
-
+        terminal_printf(35, 24, " Crafting: %d/%d ", count, maxCraftable(w, current, player->inventory));
         bool canMake = canMakeRecipe(w, current, count, player->inventory);
         if (current) {
             cy = 0;
             for (const RecipeRow &row : current->mRows) {
                 const ItemDef &partDef = w.getItemDef(row.ident);
-                if (player->inventory.qty(&partDef) < row.qty * count) {
+                int qtyHeld = player->inventory.qty(&partDef);
+                if (qtyHeld < row.qty * count) {
                     terminal_color(invalidFG);
                 } else {
                     terminal_color(validFG);
-                    terminal_put(30, cy, '+');
                 }
                 if (partDef.ident >= 0) {
-                    terminal_printf(32, cy, "%d %s", row.qty * count, partDef.name.c_str());
+                    terminal_printf(33, cy, "%d/%d %s", row.qty * count, qtyHeld, partDef.name.c_str());
+                    terminal_color(0xFFFFFFFF);
+                    terminal_put(31, cy, partDef.glyph);
                 } else {
                     terminal_printf(32, cy, "Bad item ident: %s", row.ident);
                 }
@@ -157,13 +149,8 @@ void doCrafting(World &w, Actor *player, unsigned craftingStation) {
                 if (count > 1) --count;
                 break;
             case TK_M: {
-                int maxCount = 0;
-                for (int i = 1; i < 99; ++i) {
-                    if (canMakeRecipe(w, current, i, player->inventory)) {
-                        maxCount = i;
-                    }
-                }
-                if (maxCount > 0) count = maxCount;
+                int result = maxCraftable(w, current, player->inventory);
+                if (result > 0) count = result;
                 break; }
             case TK_1:
                 count *= 10;
